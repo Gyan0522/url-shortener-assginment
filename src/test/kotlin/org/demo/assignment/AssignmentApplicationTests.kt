43f4c9a8b0d196b305b9c93e3e70fcdd89c0e0cb
Gyan Prakash <gyan_cusat07@yahoo.co.in>
2022-08-16 18:42:43 +0530

    Initial commit

---
+++ src/test/kotlin/org/demo/assignment/AssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.demo.assignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
